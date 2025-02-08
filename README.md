7235876a1ea86b5db6dfe2077d990cd032581e55
dlgudwn94 <dlgudwn82@naver.com>
2025-02-09 03:23:27 +0900

    [구성] 초기 환경 구성

---
+++ README.md
@@ -0,0 +1,1 @@
+커밋 할 때 Git: husky - DEPRECATED, Git: env: unknown option -- / 이 에러들이 뜨면서 커밋이 안 되는데 해결 방법이 있을까요?
---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import Cart from './components/Cart';
+
+function App() {
+  return (
+    <div className="p-10">
+      <Cart />
+    </div>
+  );
+}
+
+export default App;
